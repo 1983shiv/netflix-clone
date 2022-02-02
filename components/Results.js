8aac7b1886e7667588bc09b2567ea0670f2728c5
Shiv Srivastava <1983.shiv@gmail.com>
2022-02-02 11:42:53 +0530

    added flipmove for smooth loading of page

--- components/Results.js
+++ components/Results.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Thumbnail from './Thumbnail';
+import FlipMove from 'react-flip-move';
 
 const Results = ( {results} ) => {
   return (
-      <div className='px-5 py-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+      <FlipMove className='px-5 py-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
         {results.map((result) => (
           <Thumbnail key={result.id} result={result}/>
         ))}
-      </div>
+      </FlipMove>
     );
 };
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import {ThumbUpIcon} from '@heroicons/react/outline'
+import { forwardRef } from 'react';
 
-const Thumbnail = ({ result}) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original"
     // const {backdrop_path, title, overview,} = result
   return (
-        <div className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-110 hover:z-50'>
+        <div ref={ref} className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-110 hover:z-50'>
             <Image 
                 src={`${BASE_URL}${result.backdrop_path}` || `${BASE_URL}${result.poster_path}`}
                 alt={result.title}
@@ -23,6 +24,6 @@ const Thumbnail = ({ result}) => {
             </div>
         </div>
     );
-};
+});
 
 export default Thumbnail;
